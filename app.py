3bc80c5eb0fe9ca3a52ec3b2e933d77f2d11207b
Carsonliu168 <littlesevevliu@gmail.com>
2025-09-29 16:30:25 +0800

    Force redeploy v1.6

--- app.py
+++ app.py
@@ -219,4 +219,4 @@ def favicon():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     logger.info("AnAn starting... port: %s", port)
-    app.run(host="0.0.0.0", port=port, debug=DEBUG)
+    app.run(host="0.0.0.0", port=port, debug=DEBUG)# force redeploy 
